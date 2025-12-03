2465338598e31bfe17cffe67749a8f8a93c5426d
WooHyunKwon1012 <154897353+WooHyunKwon1012@users.noreply.github.com>
2025-12-03 13:38:04 +0900

    Create __init__.py

---
+++ doosan_ik_pick_and_place/config/e0509/agents/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Agent configurations for E0509 pick and place tasks."""
+
+from .rsl_rl_ppo_cfg import *
